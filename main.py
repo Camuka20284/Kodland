bda9ef327bfe0ede853ac08de6d9d5b25b513ea6
Camuka20284 <149012726+Camuka20284@users.noreply.github.com>
2023-12-02 21:55:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL" : "Bir şakaya verilen cevap",
+            "SHEESH" : "Onaylamak",
+            "CREEPY" : "Korkunç",
+            "AGGRO" : "Agresifleşmek/Sinirlenmek",
+            }
+            
